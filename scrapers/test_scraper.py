5431c641f40c26e4da1e7e734ee2656c3f4cfa9b
shalev7948 <baninshalev@gmail.com>
2026-05-02 01:07:44 +0300

    Update test_scraper.py

--- scrapers/test_scraper.py
+++ scrapers/test_scraper.py
@@ -1,5 +1,4 @@
 from scrapers.save import save
 
 def run():
-    save("Naruto", 1, "https://example.com")
-    save("One Piece", 2, "https://example.com")
+    save("Naruto", 1, "https://test.com")
